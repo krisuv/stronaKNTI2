8ad888f989f7f63541fad558a6ef90d2572ee9d8
krisuv <1krzysztof.bujalski@gmail.com>
2022-04-03 21:28:55 +0200

    wstępne dodanie media queries, bez hamburger menu

---
+++ cursorEffect.js
@@ -0,0 +1,13 @@
+const newCommand = document.querySelector(".new-command");
+const terminalUser = document.querySelector(".terminal-user");
+
+const cursor = document.createElement("span");
+cursor.setAttribute("class","cursor");
+cursor.textContent = "|";
+terminalUser.after(cursor);
+
+const cursorAnimation = () => {
+  cursor.classList.toggle("cursor-active");
+}
+
+let cursorInterval = setInterval(cursorAnimation, 400);
