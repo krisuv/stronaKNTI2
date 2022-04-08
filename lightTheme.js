76d04a6de100241388ff63139d03ab91782a13c7
krisuv <1krzysztof.bujalski@gmail.com>
2022-04-08 16:26:18 +0200

    poprawienie przycisku od zmiany motywu

--- lightTheme.js
+++ lightTheme.js
@@ -1,13 +1,17 @@
 const themeButton = document.querySelector(".light-theme-button");
-
+const themeIcon =  document.querySelector(".light-theme-button i");
+const moon = "fa-solid fa-moon";
+const sun = "fa-solid fa-sun";
 let theme = localStorage.getItem("theme");
 
 const changeTheme = () => {
   if(theme === "light") {
     document.body.classList.remove("light");
+    themeIcon.className = sun;
     theme = "dark";
   } else {
     document.body.classList.add("light");
+    themeIcon.className = moon;
     theme = "light";
   }
 
@@ -16,6 +20,7 @@ const changeTheme = () => {
 
 if(theme === "light") {
   document.body.classList.add("light");
+  themeIcon.className = moon;
 }
 
 themeButton.addEventListener('click', changeTheme);
